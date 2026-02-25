c6eab353dd1cd63786f8883ce24e0c82c6be6a22
Pierre <pierr.ducloux@gmail.com>
2026-02-25 14:45:44 +0100

    project page done

--- src/elements/project.tsx
+++ src/elements/project.tsx
@@ -1,10 +1,43 @@
 import {ProjectInterface} from "../projects.js";
 
+interface HTMLListProps {
+    items: string[];
+}
+
+export function HTMLList(props: HTMLListProps) {
+    return (
+        <ul>
+            {props.items.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+    )
+
+}
+
 export function Project(props: ProjectInterface) {
     return (
         <div className="project">
             <h2>{props.name}</h2>
             <p>{props.description}</p>
+            <div className="container flex-wrap flexcontainer-horizontal">
+                <div className="flex-wrap-no-grow flexcontainer-vertical">
+                    <h2>Specifications</h2>
+                    <HTMLList items={props.specifications} />
+                </div>
+                <div className="flex-wrap-no-grow flexcontainer-vertical">
+                    <h2>Technologies</h2>
+                    <HTMLList items={props.technologies}/>
+                </div>
+            </div>
+            <div className="container flex-wrap-no-grow flexcontainer-vertical">
+                <div className="flex-wrap-no-grow flexcontainer-vertical">
+                    <h2>challenges</h2>
+                    <HTMLList items={props.challenges}/>
+                </div>
+                <div className="flex-wrap-no-grow flexcontainer-vertical">
+                    <h2>a ameliorer</h2>
+                    <HTMLList items={props.toImprove} />
+                </div>
+            </div>
         </div>
     )
 }
--- src/projects.tsx
+++ src/projects.tsx
@@ -3,44 +3,29 @@ export interface ProjectInterface {
     smallDescription: string;
     description: string;
     image: string;
+    technologies: string[];
+    specifications: string[];
+    challenges: string[];
+    toImprove: string[];
 }
 
 export const projects: ProjectInterface[] = [
     {
-        name: "test",
-        smallDescription: "test",
-        description: "test",
-        image: "https://via.placeholder.com/150"
-    },
-    {
-        name: "test",
-        smallDescription: "test",
-        description: "test",
-        image: "https://via.placeholder.com/150"
-    },
-    {
-        name: "test",
-        smallDescription: "test",
-        description: "test",
-        image: "https://via.placeholder.com/150"
-    },
-    {
-        name: "test",
-        smallDescription: "test",
-        description: "test",
-        image: "https://via.placeholder.com/150"
-    },
-    {
-        name: "test",
-        smallDescription: "test",
-        description: "test",
-        image: "https://via.placeholder.com/150"
-    },
-    {
-        name: "test",
-        smallDescription: "test",
-        description: "test",
-        image: "https://via.placeholder.com/150"
+        name: "KFS - 1",
+        smallDescription: "Premier project d'une suite de project amenant a la creation d'un kernel",
+        description: "L'objectif de ce projet est de creer un kernel repondant a des specifications tres simples",
+        image: "https://via.placeholder.com/150",
+        technologies: [
+            "C++",
+            "i386 assembly"
+        ],
+        specifications: [
+            "Prise en charge de l'affichage en mode texte (VGA)",
+        ],
+        challenges: [
+            ""
+        ],
+        toImprove: []
     }
 ]
 
